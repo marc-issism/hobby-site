3f81eedbf93400ef6e3886c0160e8fbcde098005
Marcus Cua <moojycua@gmail.com>
2026-01-24 16:22:39 -0500

    feat: added polaroid component

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,8 +1,20 @@
+import Polaroid from "./components/Polaroid";
+import './css/bootstrap.css'
 
 function Home() {
   return(
     <>
       hi
+      <div className='container'>
+        <div className='row'>
+          {
+            [...Array(4).keys()].map(() => <div className='col-sm'><Polaroid/></div>)
+              
+          }
+        </div>
+        
+        
+      </div>
     </>
   );
 }
---
+++ src/components/Polaroid.tsx
@@ -0,0 +1,15 @@
+import '../css/bootstrap.css'
+import '../css/main.css'
+
+function Polaroid() {
+return(
+  <>
+    <div className='polaroid'>
+      <div className='polaroid-image mx-auto'>
+
+      </div>
+    </div>
+  </>
+);}
+
+export default Polaroid
